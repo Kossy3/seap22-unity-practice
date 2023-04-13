bd17c4de2272b47dc1f04de17f73444f6da94fab
SHIKINAMl <107552771+SHIKINAMl@users.noreply.github.com>
2023-04-13 19:33:00 +0900

    no message

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,20 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Score : MonoBehaviour
 {
-    private int score = 0;
-
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        Text text = this.GetComponent<Text>();
-        score = Data.data.score;
+        int myScore = Data.data.score;
+        int[] scoreRanking = GetScoreRanking(myScore);
+        
+        Transform topThreeText = this.transform.Find("TopThree").transform;
+        Transform myScoreText = this.transform.Find("MyScore").transform;
 
-        int minutes = score / 60;
-        int seconds = score % 60;
+        if (scoreRanking != null)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                DisplayScore(topThreeText.GetChild(i).transform, i+1, scoreRanking[i]);
+            } 
 
-        text.text = "スコア　" + minutes.ToString("D2") + ":" + seconds.ToString("D2");
+            //ArrayはSystemのnamespaceから
+            DisplayScore(myScoreText, Array.IndexOf(scoreRanking, myScore), myScore);
+        }
+        else
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                DisplayScore(topThreeText.GetChild(i).transform, i+1, 0);
+            }
+
+            DisplayScore(myScoreText, 0, myScore);
+        }        
+    }
+
+    private int[] GetScoreRanking(int score)
+    {
+        return null;
+    }
+
+    private void DisplayScore(Transform scoreText, int rank, int score)
+    {
+        Text rankText = scoreText.Find("Rank").GetComponent<Text>();
+        Text scoretext = scoreText.Find("Score").GetComponent<Text>();
+
+        if (score != 0)
+        {
+            int minutes = score / 60;
+            int seconds = score % 60;
+
+            scoretext.text = minutes + ":" + seconds;
+        }
+        else
+        {
+            scoretext.text = "--:--";
+        }
+
+        if (rank != 0)
+        {
+            rankText.text = rank + "位";
+        }
+        else
+        {
+            rankText.text = "-位";
+        }
     }
 }
